54a38363de69ff194c2cbbd5f52650e84136ba7d
RayCPreston <raycpreston@gmail.com>
2025-02-03 06:34:56 -0600

    Add login operation

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using DTH.API.Models;
 using DTH.API.Services.UserServices;
 using DTH.API.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DTH.API.Controllers
@@ -19,7 +20,22 @@ namespace DTH.API.Controllers
             _createUserService = createUserService;
         }
 
-        [HttpPost]
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Exception occurred in UserController.Login {ex.Message}");
+                return StatusCode(500, ErrorUtil.CreateError("005", ex.Message));
+            }
+        }
+
+            [HttpPost]
         [Route("create/user")]
         public IActionResult CreateUser([FromBody] User user)
         {
